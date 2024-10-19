87d5c2cbe67980c8a7b6b76b586999d23e38ff01
AmitxParmar <amitparmar901@gmail.com>
2024-10-20 03:57:01 +0530

    tweak: config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
-dotenv.config();
 
 // Get the directory name from the current module's URL
 const __filename = fileURLToPath(import.meta.url);
@@ -12,9 +10,6 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -23,9 +18,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
+        target: "https://job-portal-mern-tw7s.onrender.com",
         secure: false,
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
