efd1c4cecd7c8db8b94ac414a99695d63d4b961d
AmitxParmar <amitparmar901@gmail.com>
2024-10-20 02:47:35 +0530

    more fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
       "/api": {
         target: process.env.VITE_API_URL,
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
