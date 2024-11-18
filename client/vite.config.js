c746db9ba9a6468d7b8b06c52c381217c43f178a
AmitxParmar <amitparmar901@gmail.com>
2024-11-18 22:35:35 +0530

    fix: local development api issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,16 +15,36 @@ export default defineConfig(({ mode }) => {
   /*   const PORT = `${env.VITE_PORT ?? "3000"}`; */
 
   return {
-    /*     server: {
+    server: {
       proxy: {
         "/api": {
           target: API_URL,
           changeOrigin: true,
           secure: false,
+          ws: true,
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log(
+                "Sending Request to the Target:",
+                req.method,
+                req.url
+              );
+            });
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url
+              );
+            });
+          },
         },
       },
-      port: PORT,
-    }, */
+    },
+    /*     port: PORT, */
     build: {
       outDir: "dist",
       define: {
